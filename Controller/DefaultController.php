e56f530da3de4943fde76b98aaa0d880e56673a2
Gerard TOKO <gerardtoko@gmail.com>
2013-02-26 23:29:24 +0100

    init projet

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GT\AuthBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('GTAuthBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ GTAuthBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GT\AuthBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GTAuthBundle extends Bundle
+{
+}
